7acf7386c7edd36332d5701b2a74f3033c8d029c
Isaac-tech-dev <ayeniisaac1on1@gmail.com>
2023-06-30 22:22:34 +0100

    Fourth Commit

---
+++ src/main/java/com/isaac/springdemo/student/InMemoryStudentService.java
@@ -0,0 +1,40 @@
+package com.isaac.springdemo.student;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class InMemoryStudentService implements StudentService{
+
+    private final InMemoryStudentDao dao;
+
+    public InMemoryStudentService(InMemoryStudentDao dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    public Student save(Student s) {
+        return dao.save(s);
+    }
+
+    @Override
+    public List<Student> findAllStudents(){
+        return dao.findAllStudents();
+    }
+
+    @Override
+    public Student findByEmail(String email) {
+        return dao.findByEmail(String.valueOf(email));
+    }
+
+    @Override
+    public void delete(String email) {
+        dao.delete(email);
+    }
+
+    @Override
+    public Student update(Student s) {
+        return dao.update(s);
+    }
+}
--- src/main/java/com/isaac/springdemo/student/StudentService.java
+++ src/main/java/com/isaac/springdemo/student/StudentService.java
@@ -1,30 +1,20 @@
 package com.isaac.springdemo.student;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Service
-public class StudentService {
-    @GetMapping
-    public List<Student> findAllStudents() {
-        return List.of(
-                new Student(
-                        "Isaac",
-                        "Ayeni",
-                        LocalDate.now(),
-                        "isaac@gmail.com",
-                        28
-                ),
-                new Student(
-                        "Tobi",
-                        "Badejo",
-                        LocalDate.now(),
-                        "tobi@gmail.com",
-                        22
-                )
-        );
-    }
+public interface StudentService {
+
+
+    Student save(Student s);
+
+    List<Student> findAllStudents();
+
+    Student findByEmail (String email);
+
+    void delete(String email);
+
+    Student update(Student s);
 }
